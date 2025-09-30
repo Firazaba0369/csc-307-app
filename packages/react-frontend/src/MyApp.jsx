b6d94f79144c25f16dd55ddfb3a58e5bdb21276c
Franky Irazaba <firazaba0369@gmail.com>
2025-09-29 23:00:15 -0700

    Added form to input data functionality

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,36 +1,10 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    } 
-    
-  ]);
-
-  return (
-  <div className="container">
-    <Table
-      characterData={characters}
-      removeCharacter={removeOneCharacter}
-    />
-  </div>
-  );
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -38,6 +12,16 @@ function MyApp() {
     });
     setCharacters(updated);
   }
+
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+      <Form />
+    </div>
+  );
 }
 
 export default MyApp;

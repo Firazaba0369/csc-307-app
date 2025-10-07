8d1ef072595b6d7cc7f3fbdeca1caee2c17fa772
Franky Irazaba <firazaba0369@gmail.com>
2025-10-07 10:12:29 -0700

    Added first HTTP request to frontend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -6,6 +6,11 @@ import Form from "./Form";
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }  
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -16,6 +21,13 @@ function MyApp() {
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+  useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+  }, [] );
 
   return (
     <div className="container">
